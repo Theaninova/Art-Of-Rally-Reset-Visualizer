81f546215100d43ba108b1a49f7406515335e002
Wieland Schöbl <wulkanat@gmail.com>
2022-01-17 12:45:16 +0100

    Switch to Unity Mod Manager

--- ArtOfRallyResetVisualizer/Class1.cs
+++ ArtOfRallyResetVisualizer/Class1.cs
@@ -6,66 +6,88 @@ namespace ArtOfRallyResetVisualizer
 {
     public class ResetVisualizer
     {
-        public class ResetVisualizerState
-        {
-            public List<GameObject> WaypointVisualizers = new List<GameObject>();
-            public List<GameObject> NoGoVisualizers = new List<GameObject>();
-        }
+	    private static readonly int SrcBlend = Shader.PropertyToID("_SrcBlend");
+	    private static readonly int DstBlend = Shader.PropertyToID("_DstBlend");
+	    private static readonly int ZWrite = Shader.PropertyToID("_ZWrite");
+	    private static readonly int Mode = Shader.PropertyToID("_Mode");
+	    private static readonly int SpecularHighlights = Shader.PropertyToID("_SpecularHighlights");
+	    private static readonly int GlossyReflections = Shader.PropertyToID("_GlossyReflections");
 
-        static void SetTransparentColor(GameObject gameObject, Color color)
+	    private const string NoGoVisualizersName = "NoGoVisualizers";
+	    private const string WaypointVisualizersName = "WaypointVisualizersName";
+
+	    private static void SetTransparentColor(GameObject gameObject, Color color)
         {
-			MeshRenderer meshRenderer = gameObject.GetComponent(typeof(MeshRenderer)) as MeshRenderer;
-			meshRenderer.receiveShadows = false;
-			meshRenderer.material.SetInt("_SrcBlend", 1);
-			meshRenderer.material.SetInt("_DstBlend", 10);
-			meshRenderer.material.SetInt("_ZWrite", 0);
-			meshRenderer.material.DisableKeyword("_ALPHATEST_ON");
-			meshRenderer.material.DisableKeyword("_ALPHABLEND_ON");
-			meshRenderer.material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
-			meshRenderer.material.renderQueue = 3000;
-			meshRenderer.material.SetFloat("_Mode", 3f);
-			meshRenderer.material.color = color;
-			meshRenderer.material.EnableKeyword("_SpecularHighlights");
-			meshRenderer.material.EnableKeyword("_GlossyReflections");
-			meshRenderer.material.SetFloat("_SpecularHighlights", 0f);
-			meshRenderer.material.SetFloat("_GlossyReflections", 0f);
+			var meshRenderer = gameObject.GetComponent(typeof(MeshRenderer)) as MeshRenderer;
+			meshRenderer!.receiveShadows = false;
+			var material = meshRenderer.material;
+			material.SetInt(SrcBlend, 1);
+			material.SetInt(DstBlend, 10);
+			material.SetInt(ZWrite, 0);
+			// ReSharper disable once StringLiteralTypo
+			material.DisableKeyword("_ALPHATEST_ON");
+			// ReSharper disable once StringLiteralTypo
+			material.DisableKeyword("_ALPHABLEND_ON");
+			// ReSharper disable once StringLiteralTypo
+			material.EnableKeyword("_ALPHAPREMULTIPLY_ON");
+			material.renderQueue = 3000;
+			material.SetFloat(Mode, 3f);
+			material.color = color;
+			material.EnableKeyword("_SpecularHighlights");
+			material.EnableKeyword("_GlossyReflections");
+			material.SetFloat(SpecularHighlights, 0f);
+			material.SetFloat(GlossyReflections, 0f);
 		}
 
         [HarmonyPatch(typeof(OutOfBoundsManager), nameof(OutOfBoundsManager.Start))]
         [HarmonyPostfix]
-        public void Start(OutOfBoundsManager __instance, out ResetVisualizerState __state)
+        // ReSharper disable twice InconsistentNaming
+        public void Start(OutOfBoundsManager __instance, float ___RESET_DISTANCE)
         {
-            __state = new ResetVisualizerState();
-
-            Transform resets = null;
+	        Transform resets = null;
             try
             {
                 resets = GameObject.Find("ResetZones").transform;
             }
             catch
             {
+	            // ignored
             }
 
-			if (resets != null)
-			{
-				foreach (object obj in resets)
+            if (resets != null)
+            {
+	            var noGoParent = new GameObject(NoGoVisualizersName);
+	            Object.Instantiate(noGoParent);
+				foreach (var obj in resets)
 				{
-					SphereCollider resetObj = ((Transform)obj).gameObject.GetComponent<SphereCollider>();
-					GameObject noGoVisualizer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-					__state.NoGoVisualizers.Add(noGoVisualizer);
+					var resetObj = ((Transform)obj).gameObject.GetComponent<SphereCollider>();
+					var noGoVisualizer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+					
+					noGoVisualizer.SetActive(false);
+					noGoVisualizer.transform.SetParent(noGoParent.transform);
 					noGoVisualizer.transform.position = resetObj.transform.position;
-					noGoVisualizer.transform.localScale = new Vector3(resetObj.radius * 2f, resetObj.radius * 2f, resetObj.radius * 2f);
+
+					var radius = resetObj.radius * 2f;
+					noGoVisualizer.transform.localScale = new Vector3(radius, radius, radius);
+
 					((Collider)noGoVisualizer.GetComponent(typeof(Collider))).isTrigger = true;
 					SetTransparentColor(noGoVisualizer, new Color(1f, 0f, 0f, 0.7f));
 				}
 			}
 
-			foreach (Vector3 transform in __instance.GetWaypointList())
+            var waypointParent = new GameObject(WaypointVisualizersName);
+            Object.Instantiate(waypointParent);
+			foreach (var transform in __instance.GetWaypointList())
             {
-				GameObject waypointVisualizer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+				var waypointVisualizer = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
 				waypointVisualizer.SetActive(false);
+				waypointVisualizer.transform.SetParent(waypointParent.transform);
 				waypointVisualizer.transform.position = transform;
-				waypointVisualizer.transform.localScale = new Vector3(34f, 34f, 34f);
+
+				var radius = ___RESET_DISTANCE * 2f;
+				waypointVisualizer.transform.localScale = new Vector3(radius, radius, radius);
+
 				((Collider)waypointVisualizer.GetComponent(typeof(Collider))).isTrigger = true;
 				SetTransparentColor(waypointVisualizer, new Color(1f, 0.8f, 0.8f, 0.1f));
 			}
