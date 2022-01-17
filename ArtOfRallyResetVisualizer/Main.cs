5dce3d95c26ac6dd4f56524a479dcdd7fa5fdd5f
Wieland Schöbl <wulkanat@gmail.com>
2022-01-17 14:14:22 +0100

    Change patch type

--- ArtOfRallyResetVisualizer/Main.cs
+++ ArtOfRallyResetVisualizer/Main.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using HarmonyLib;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityModManagerNet;
 
 namespace ArtOfRallyResetVisualizer
@@ -9,14 +10,20 @@ namespace ArtOfRallyResetVisualizer
     public class Main
     {
         public static Settings Settings;
+        private static bool _patched = false;
 
         // ReSharper disable once ArrangeTypeMemberModifiers
         // ReSharper disable once UnusedMember.Local
         static bool Load(UnityModManager.ModEntry modEntry)
         {
-            var harmony = new Harmony(modEntry.Info.Id);
-            harmony.PatchAll(Assembly.GetExecutingAssembly());
-
+            SceneManager.sceneLoaded += (arg0, mode) =>
+            {
+                if (_patched) return;
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+                _patched = true;
+            };
+                
             Settings = new Settings();
             modEntry.OnGUI = entry => Settings.Draw(entry);
             modEntry.OnSaveGUI = entry => Settings.Save(entry);
--- ArtOfRallyResetVisualizer/Settings.cs
+++ ArtOfRallyResetVisualizer/Settings.cs
@@ -5,8 +5,9 @@ namespace ArtOfRallyResetVisualizer
 {
     public class Settings : UnityModManager.ModSettings, IDrawable
     {
-        [Draw("Show Reset Zones")] public bool ShowResetZones = false;
-        [Draw("Show Waypoints")] public bool ShowWaypoints = false;
+        [Header("Visualizers")]
+        [Draw("Reset Zones")] public bool ShowResetZones = false;
+        [Draw("Waypoints")] public bool ShowWaypoints = false;
 
         public void OnChange()
         {
