e3beb3f9e87f9961d011167c9db3d7cfe552662a
Wieland Schöbl <wulkanat@gmail.com>
2022-01-18 14:44:18 +0100

    start hit mode

--- ArtOfRallyResetVisualizer/ResetVisualizerSettings.cs
+++ ArtOfRallyResetVisualizer/ResetVisualizerSettings.cs
@@ -4,12 +4,17 @@ using UnityModManagerNet;
 
 namespace ArtOfRallyResetVisualizer
 {
+    public enum RenderMode { Off, OnHit, Always }
+    
     public class ResetVisualizerSettings : UnityModManager.ModSettings, IDrawable
     {
-        [Header("Visualizers")] [Draw("Reset Zones")]
-        public bool ShowResetZones = false;
+        [Header("Modes")] [Draw(DrawType.ToggleGroup)]
+        public RenderMode RenderMode = RenderMode.Off;
 
-        [Draw("Waypoints")] public bool ShowWaypoints = false;
+        [Header("Visualizers")] [Draw("Reset Zones")]
+        public bool ShowResetZones = true;
+
+        [Draw("Waypoints")] public bool ShowWaypoints = true;
 
         [Header("Colors")] [Draw("Reset Color")]
         public Color ResetColor = new Color(1f, 0f, 0f, 0.7f);
